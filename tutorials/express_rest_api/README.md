e34064d3f0554c64ba22204a6793ee596c1a3129
BenjaminOmar <benjaminjoof01@gmail.com>
2023-01-25 13:54:53 +0100

    Initated the express app
    - Inititated the express app
    - set up node and nodemon
    - Learned about how to d basic get and post requests.

---
+++ tutorials/express_rest_api/README.md
@@ -0,0 +1,7 @@
+Following a course to build a rest api with Npde js & express
+
+by:
+    Programming with mosh
+
+link:
+    https://www.youtube.com/watch?v=pKd0Rpw7O48&t=11s&ab_channel=ProgrammingwithMosh
---
+++ tutorials/express_rest_api/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses); 
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course){ 
+        res.status(404).send('The course with the given ID was not found.');
+    }
+    else {
+        res.send(course);
+    }
+})
+
+// PORT
+const port = process.env.PORT || 3000; // will listen to PORT inn production, but for now we are listening to 3000 locally
+
+app.listen(port, () => console.log(`listening on port ${port}`));
